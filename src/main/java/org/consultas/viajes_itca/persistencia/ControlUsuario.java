af7f0531035c2007dffd213bf7c42c4444475ddc
alex <am18007@ues.edu.sv>
2024-10-24 19:25:32 -0600

    agregano control usuario

---
+++ src/main/java/org/consultas/viajes_itca/persistencia/ControlUsuario.java
@@ -0,0 +1,30 @@
+package org.consultas.viajes_itca.persistencia;
+
+import javax.persistence.EntityManagerFactory;
+import org.consultas.viajes_itca.entity.Usuarios;
+
+public class ControlUsuario extends AbstractJpaController<Usuarios>{
+    private  EntityManagerFactory emf;
+    UsuariosJpaController control=null;
+
+    public ControlUsuario(EntityManagerFactory emf, Class<Usuarios> entityClass) {
+        super(emf, entityClass);
+        this.emf = emf;
+        control = new UsuariosJpaController(emf);
+    }
+
+    @Override
+    protected Object getId(Usuarios entity) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+    public Usuarios findUsuariosEmail(String email) {
+        return control.findUsuarios(email);
+    }
+
+    public Usuarios findUsuariosEmailPass(String email, String password) {
+        return control.finUsuarios(email, password);
+    }
+
+}
