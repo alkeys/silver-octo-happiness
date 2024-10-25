f2be8702083f2984f2dc036efad8191d180f7f18
alex <am18007@ues.edu.sv>
2024-10-24 19:39:52 -0600

    modificando server

---
+++ src/main/java/org/consultas/viajes_itca/control/Seccion.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package org.consultas.viajes_itca.control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author alex
+ */
+@WebServlet(name = "Seccion", urlPatterns = {"/Seccion"})
+public class Seccion extends HttpServlet {
+
+public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Seccion</h1>");
+        out.println("</body></html>");
+    }
+    public void destroy() {
+    }
+}
